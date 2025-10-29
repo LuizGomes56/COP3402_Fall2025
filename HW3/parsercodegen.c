c3ac8a2b22cb94653f80dc587cf59d80bbfd2b6e
LuizGomes56 <luizgustavodgomes@gmail.com>
2025-10-28 23:51:37 -0400

    Added documentation for code.

--- HW3/parsercodegen.c
+++ HW3/parsercodegen.c
@@ -1,29 +1,38 @@
 /*
 Assignment:
-HW3 - Parser and Code Generator for PL/0
+    HW3 - Parser and Code Generator for PL/0
+
 Author(s): Luiz Gustavo Santana Dias Gomes
 Language: C (only)
+
 To Compile:
-Scanner:
-gcc -O2 -std=c11 -o lex lex.c
-Parser/Code Generator:
-gcc -O2 -std=c11 -o parsercodegen parsercodegen.c
-To Execute (on Eustis):
-./lex <input_file.txt>
-./parsercodegen
-where:
-<input_file.txt> is the path to the PL/0 source program
+    Scanner:
+        gcc -O2 -std=c11 -o lex lex.c
+    Parser/Code Generator:
+        gcc -O2 -std=c11 -o parsercodegen parsercodegen.c
+    To Execute (on Eustis):
+        ./lex <input_file.txt>
+        ./parsercodegen
+    where:
+        <input_file.txt> is the path to the PL/0 source program
+
 Notes:
-- lex.c accepts ONE command-line argument (input PL/0 source file)
-- parsercodegen.c accepts NO command-line arguments
-- Input filename is hard-coded in parsercodegen.c
-- Implements recursive-descent parser for PL/0 grammar
-- Generates PM/0 assembly code (see Appendix A for ISA)
-- All development and testing performed on Eustis
-Class: COP3402 - System Software - Fall 2025
+    - lex.c accepts ONE command-line argument (input PL/0 source file)
+    - parsercodegen.c accepts NO command-line arguments
+    - Input filename is hard-coded in parsercodegen.c
+    - Implements recursive-descent parser for PL/0 grammar
+    - Generates PM/0 assembly code (see Appendix A for ISA)
+    - All development and testing performed on Eustis
+
+    Class: COP3402 - System Software - Fall 2025
+
 Instructor: Dr. Jie Lin
 Due Date: Friday, October 31, 2025 at 11:59 PM ET
 */
+
+// ! No AI usage; Sources = GOOGLE
+// ! This is a literal translation I did to my Rust code
+// ! (commented at the end of this file)
 
 #include <ctype.h>
 #include <stdio.h>
@@ -33,12 +42,17 @@ Due Date: Friday, October 31, 2025 at 11:59 PM ET
 #define MAX_SYMBOL_TABLE_SIZE 500
 #define MAX_PCODE_SIZE 5000
 
+/// @brief Port the MayFail type from Rust to C
 typedef struct MayFail {
     int is_error;
+    /// @brief Can be anything after all. A pointer that points to anything
     void *value;
+    /// @brief If `is_error` is true, this is a pointer to a string
     const char *error_message;
 } MayFail;
 
+/// @brief I'd rather use my own struct but this was provided by the assignment
+/// and I'm not sure if I can change it.
 typedef struct symbol {
     int kind;      // const = 1, var = 2, proc = 3
     char name[12]; // name up to 11 chars
@@ -49,8 +63,12 @@ typedef struct symbol {
 } symbol;
 
 symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+
+/// All `self` variables were moved to a static environment. Rust controls
+/// the length of a Vec automatically, but C doesn't have it
 static int SYMBOL_TABLE_LEN = 0;
 
+/// Error codes defined in the assignment details
 char *throw(int code) {
     switch (code) {
     case 1:
@@ -83,6 +101,7 @@ char *throw(int code) {
         return "Error: right parenthesis must follow left parenthesis";
     case 15:
         return "Error: arithmetic equations must contain operands, parentheses, numbers, or symbols";
+    /// Custom error I added since I noticed that this "if-then" error was missing
     case 16:
         return "Error: if must have a condition and be followed by fi";
     default:
@@ -90,12 +109,14 @@ char *throw(int code) {
     }
 }
 
+/// @brief Symbol kind to be placed in the symbol table
 typedef enum SymbolKind {
     SymbolKind_Const = 1,
     SymbolKind_Var,
     SymbolKind_Proc,
 } SymbolKind;
 
+/// @brief Operation codes for OPR instruction
 typedef enum OprCode {
     OprCode_RTN,
     OprCode_ADD,
@@ -111,12 +132,14 @@ typedef enum OprCode {
     OprCode_EVEN,
 } OprCode;
 
+/// @brief Any instruction SYS
 typedef enum Syscall {
     Syscall_Write = 1,
     Syscall_Read,
     Syscall_Halt,
 } Syscall;
 
+/// @brief Tokens defined the same way as in lex.c
 typedef enum TokenType {
     TokenType_Skip = 1,
     TokenType_Ident,
@@ -154,6 +177,7 @@ typedef enum TokenType {
     TokenType_Even,
 } TokenType;
 
+/// @brief Self explanatory
 typedef enum Instruction {
     Instruction_LIT = 1,
     Instruction_OPR,
@@ -166,26 +190,34 @@ typedef enum Instruction {
     Instruction_SYS,
 } Instruction;
 
+/// @brief Same struct as defined in HW1; Holds some OP code, L and M
 typedef struct Register {
     Instruction OP;
     int L;
     int M;
 } Register;
 
+/// @brief Same definition used in Rust. `meta` is used only for the following types:
+/// `TokenType::Number` or `TokenType::Ident` since they have their names after the token number
 typedef struct Token {
     TokenType kind;
     char meta[12];
 } Token;
 
+/// @brief Emulating Rust behavior, but does not have `pcode` and `symbol_table` since they're
+/// static, so they're accessible anywhere.
 typedef struct TokenStream {
     int iteration;
     Token *tokens;
     int len_tokens;
 } TokenStream;
 
+/// @brief Previously defined in TokenStream struct (Rust)
 static Register pcode[MAX_PCODE_SIZE];
 
 static int PCODE_LEN = 0;
+
+/// @brief Same as `self.iteration`
 static int TS_ITERATION = 0;
 
 Token ts_token(TokenStream *self);
@@ -202,9 +234,11 @@ MayFail ts_expression(TokenStream *self);
 MayFail ts_term(TokenStream *self);
 MayFail ts_factor(TokenStream *self);
 MayFail ts_get_token(TokenStream *self, int iteration);
-MayFail ts_get_symbol(TokenStream *self, int symbol_index);
 int token_get_number(Token token);
 
+/// @brief Receives a pointer to anything and returns a MayFail type.
+/// @param value Intended to be an `int` or `String`
+/// @return `struct MayFail`
 MayFail Ok(void *value) {
     MayFail may_fail = {
         .value = value,
@@ -213,6 +247,9 @@ MayFail Ok(void *value) {
     return may_fail;
 }
 
+/// @brief Receives any message and returns a MayFail type with Err variant
+/// @param error_message Usually from function `throw`, but can be a literal string as well
+/// @return `struct MayFail`
 MayFail Err(char *error_message) {
     MayFail may_fail;
     may_fail.value = NULL;
@@ -221,6 +258,7 @@ MayFail Err(char *error_message) {
     return may_fail;
 }
 
+/// @brief Print the symbol table. `st` stands for Symbol Table
 void st_print() {
     printf(
         "Symbol Table: \n\n%-4s | %-11s | %-5s | %-5s | %-7s | %-4s\n",
@@ -235,19 +273,22 @@ void st_print() {
     }
     puts("");
     for (int i = 0; i < SYMBOL_TABLE_LEN; i++) {
-        symbol symbol = symbol_table[i];
+        symbol sym = symbol_table[i];
 
         printf(
-            "%4s | %11s | %5d | %5d | %7d | %4d\n",
-            symbol.kind,
-            symbol.name,
-            symbol.val,
-            symbol.level,
-            symbol.addr,
-            symbol.mark);
+            "%4d | %11s | %5d | %5d | %7d | %4d\n",
+            sym.kind,
+            sym.name,
+            sym.val,
+            sym.level,
+            sym.addr,
+            sym.mark);
     }
 }
 
+/// @brief Checks if a symbol is in the symbol table
+/// @param name Symbol name. May not repeat since variables and constants are in the same scope
+/// @return `int` 1 for true, 0 for false
 int st_has_symbol(char *name) {
     for (int i = 0; i < SYMBOL_TABLE_LEN; i++) {
         if (strcmp(symbol_table[i].name, name) == 0) {
@@ -257,6 +298,9 @@ int st_has_symbol(char *name) {
     return 0;
 }
 
+/// @brief Returns the symbol at the given index. If index does not exist, throws.
+/// @param index Index where the wanted symbol is at
+/// @return `struct MayFail` with `void*` being of type `symbol`
 MayFail st_get_index(int index) {
     if (index < 0 || index >= SYMBOL_TABLE_LEN) {
         return Err("Index out of bounds");
@@ -264,6 +308,11 @@ MayFail st_get_index(int index) {
     return Ok(&(symbol_table[index]));
 }
 
+/// @brief Assigns to `out_index` the index of the symbol. Can fail.
+/// @param name Name of the ident to be searched for
+/// @param code Code to be called in function `throw` if an error occur.
+/// @param out_index Variable where the index will be assigned, if it exists
+/// @return `struct MayFail` with `void*` null
 MayFail st_find_index_or_throw(const char *name, int code, int *out_index) {
     for (int i = 0; i < SYMBOL_TABLE_LEN; i++) {
         if (strcmp(symbol_table[i].name, name) == 0) {
@@ -274,11 +323,18 @@ MayFail st_find_index_or_throw(const char *name, int code, int *out_index) {
     return Err(throw(code));
 }
 
+/// @brief Inserts a symbol to the symbol table. Can't fail.
+/// @param symbol Symbol to be inserted
 void st_push_symbol(symbol symbol) {
     symbol_table[SYMBOL_TABLE_LEN] = symbol;
     SYMBOL_TABLE_LEN++;
 }
 
+/// @brief Pushes a constant to the symbol table
+/// @param name Constant ident name
+/// @param value Its literal value
+/// @return `struct MayFail`. Fails if the symbol already exists. `void*` is of type `int`
+/// representing the index of the symbol that was just inserted
 MayFail st_push_const(char *name, int value) {
     int out_index = 0;
     MayFail fail = st_find_index_or_throw(name, 3, &out_index);
@@ -298,6 +354,11 @@ MayFail st_push_const(char *name, int value) {
     }
 }
 
+/// @brief Pushes a variable to the symbol table
+/// @param name Variable ident name
+/// @param addr Address of the variable
+/// @return `struct MayFail`. Fails if the symbol already exists. `void*` is of type `int`
+/// representing the index of the symbol that was just inserted
 MayFail st_push_var(char *name, int addr) {
     int out_index = 0;
     MayFail fail = st_find_index_or_throw(name, 3, &out_index);
@@ -317,6 +378,7 @@ MayFail st_push_var(char *name, int addr) {
     }
 }
 
+/// @brief Prints the assembly code
 void pcode_print() {
     printf("Assembly Code:\n\n%4s%8s%8s%8s\n", "Line", "OP", "L", "M");
     for (int i = 0; i < PCODE_LEN; i++) {
@@ -371,10 +433,16 @@ void pcode_print() {
     }
 }
 
+/// @brief Multiplies by 3 the current `pcode` array length to get the `JPC` offset
+/// @return The `JPC` offset
 int ts_jpc_offset() {
     return PCODE_LEN * 3;
 }
 
+/// @brief Adds an instruction to the `pcode` array
+/// @param self `TokenStream` adapt
+/// @param instr Instruction code
+/// @param M M value
 void ts_emit(TokenStream *self, Instruction instr, int M) {
     Register reg = {
         .OP = instr,
@@ -385,6 +453,10 @@ void ts_emit(TokenStream *self, Instruction instr, int M) {
     PCODE_LEN += 1;
 }
 
+/// @brief Returns the provided `iteration` index in the token list. Fails if does not exist,
+/// and panics if the index is out of bounds
+/// @param iteration Index of the token to be searched for
+/// @return `struct MayFail` with `void*` being of type `Token`
 MayFail ts_get_token(TokenStream *self, int iteration) {
     if (iteration >= self->len_tokens) {
         printf("%s", "Failed assert that Token Stream iteration != Token len");
@@ -393,6 +465,8 @@ MayFail ts_get_token(TokenStream *self, int iteration) {
     return Ok(&(self->tokens[iteration]));
 }
 
+/// @brief Returns the current iteration's token. Panics if the current iteration is invalid.
+/// @return `struct Token`
 Token ts_token(TokenStream *self) {
     MayFail fail = ts_get_token(self, TS_ITERATION);
     if (fail.is_error) {
@@ -402,15 +476,22 @@ Token ts_token(TokenStream *self) {
     return *(Token *)fail.value;
 }
 
+/// @brief Calls function `ts_token` and accesses the `kind` field
+/// @return `enum TokenType`
 TokenType ts_kind(TokenStream *self) {
     return ts_token(self).kind;
 }
 
+/// @brief Passes to the next token, and returns the next token type. Panics if index is invalid
+/// @return `enum TokenType`
 TokenType ts_next(TokenStream *self) {
     TS_ITERATION += 1;
     return ts_kind(self);
 }
 
+/// @brief Receives an `out_index` and assigns to it, if successful, the index of the symbol in Symbol Table
+/// @param out_index Where the index will be assigned in case of success
+/// @return `struct MayFail` with `void*` null
 MayFail ts_get_symbol_index(TokenStream *self, int *out_index) {
     Token token = ts_token(self);
     char *meta = token.meta;
@@ -421,10 +502,8 @@ MayFail ts_get_symbol_index(TokenStream *self, int *out_index) {
     return Ok(NULL);
 }
 
-MayFail ts_get_symbol(TokenStream *self, int symbol_index) {
-    return st_get_index(symbol_index);
-}
-
+/// @brief Starts to parse the token stream
+/// @return `struct MayFail` with `void*` null
 MayFail ts_program(TokenStream *self) {
     TokenType last = self->tokens[self->len_tokens - 1].kind;
     if (last != TokenType_Period) {
@@ -438,6 +517,8 @@ MayFail ts_program(TokenStream *self) {
     }
 }
 
+/// @brief Parses `block`
+/// @return `struct MayFail` with `void*` null
 MayFail ts_block(TokenStream *self) {
     MayFail const_declaration_fail = ts_const_declaration(self);
 
@@ -511,6 +592,9 @@ MayFail ts_const_declaration(TokenStream *self) {
     return Ok(NULL);
 }
 
+/// @brief Parses `var_declaration` and assigns to `count` the number of variables found
+/// @param count Pointer to the number of variables to be assigned
+/// @return `struct MayFail` with `void*` null
 MayFail ts_var_declaration(TokenStream *self, int *count) {
     if (ts_kind(self) == TokenType_Var) {
         do {
@@ -554,7 +638,7 @@ MayFail ts_statement(TokenStream *self) {
             return get_symbol_fail;
         }
 
-        MayFail get_symbol_fail_2 = ts_get_symbol(self, symbol_index_s0);
+        MayFail get_symbol_fail_2 = st_get_index(symbol_index_s0);
         if (get_symbol_fail_2.is_error) {
             return get_symbol_fail_2;
         }
@@ -666,7 +750,7 @@ MayFail ts_statement(TokenStream *self) {
             return symbol_index_fail;
         }
 
-        MayFail get_Symbol_fail_3 = ts_get_symbol(self, symbol_index_s1);
+        MayFail get_Symbol_fail_3 = st_get_index(symbol_index_s1);
         if (get_Symbol_fail_3.is_error) {
             return get_Symbol_fail_3;
         }
@@ -780,6 +864,7 @@ MayFail ts_expression(TokenStream *self) {
         }
 
         ts_emit(self, Instruction_OPR, OprCode_NEG);
+
         while (ts_kind(self) == TokenType_Plus || ts_kind(self) == TokenType_Minus) {
             if (ts_kind(self) == TokenType_Plus) {
                 ts_next(self);
@@ -810,8 +895,6 @@ MayFail ts_expression(TokenStream *self) {
         if (fail_term.is_error) {
             return fail_term;
         }
-
-        ts_emit(self, Instruction_OPR, OprCode_ADD);
 
         while (ts_kind(self) == TokenType_Plus || ts_kind(self) == TokenType_Minus) {
             if (ts_kind(self) == TokenType_Plus) {
@@ -880,7 +963,7 @@ MayFail ts_factor(TokenStream *self) {
         }
 
         symbol_table[symbol_index].mark = 1;
-        MayFail symbol_fail = ts_get_symbol(self, symbol_index);
+        MayFail symbol_fail = st_get_index(symbol_index);
         if (symbol_fail.is_error) {
             return symbol_fail;
         }
@@ -918,6 +1001,8 @@ MayFail ts_factor(TokenStream *self) {
     return Ok(NULL);
 }
 
+/// @brief Returns -1 if token's meta is not a valid number, and the number otherwise
+/// @return Parsing of token's meta to integer. Same as `token.meta.parse::<i32>().unwrap_or(-1)`
 int token_get_number(Token token) {
     if (token.meta == NULL) {
         return -1;
@@ -926,6 +1011,9 @@ int token_get_number(Token token) {
     }
 }
 
+/// @brief Checks if a string contains only whitespace kind. Builtin function in Rust.
+/// @param s The string (intended to be a line of the file)
+/// @return `int` 1 for true, 0 for false
 int is_blank_or_whitespace_only(const char *s) {
     for (; *s; ++s) {
         if (!isspace((unsigned char)*s)) {
@@ -935,6 +1023,8 @@ int is_blank_or_whitespace_only(const char *s) {
     return 1;
 }
 
+/// @brief Strips newlines from the end of a string. Like `.trim()`
+/// @param s The string
 void rstrip_newline(char *s) {
     int n = strlen(s);
     while (n > 0 && (s[n - 1] == '\n' || s[n - 1] == '\r')) {
@@ -942,6 +1032,7 @@ void rstrip_newline(char *s) {
     }
 }
 
+/// @brief Converts a line to a Token struct.
 Token token_from_str(const char *line) {
     Token token = {.kind = 0, .meta = ""};
     int kind;
@@ -959,18 +1050,25 @@ int main() {
     FILE *file_ptr = fopen("tokens.txt", "r");
 
     Token *tokens = NULL;
+    /// Capacity and length of token list initialized to 0
     int len = 0, cap = 0;
 
+    /// @brief A line should never have more than ~20 characters but 1024 is just in case
     char line[1024];
     while (fgets(line, sizeof line, file_ptr)) {
+        /// same as .trim()
         rstrip_newline(line);
+        /// same as .filter(char::is_whitespace)
         if (line[0] == '\0' || is_blank_or_whitespace_only(line)) {
             continue;
         }
 
+        /// same as .map(Token::from)
         Token this_token = token_from_str(line);
 
+        /// Realloc if the buffer has run out of space
         if (len == cap) {
+            /// If capacity is zero, multiply by 2 make no sense
             int new_cap = cap ? cap * 2 : 64;
             Token *tmp = (Token *)realloc(tokens, new_cap * sizeof *tokens);
             if (!tmp) {
@@ -987,22 +1085,27 @@ int main() {
 
     fclose(file_ptr);
 
-    // for (int i = 0; i < len; ++i) {
-    //     printf("Token %zu: kind=%d meta=\"%s\"\n", i, tokens[i].kind, tokens[i].meta);
-    // }
-
+    /// Same as creating the TokenStream type: TokenStream::from(tokens)
     TokenStream token_stream = {.tokens = tokens, .len_tokens = len};
 
+    /// Emit the hardcoded instruction of JMP 0 3
     ts_emit(&token_stream, Instruction_JMP, 3);
 
+    /// Start program execution
     MayFail program = ts_program(&token_stream);
     if (program.is_error) {
+        /// Errors will be propagated as value in a chain, same as in Rust's `?` operator.
+        /// If any error in any function that returns MayFail is truthy, this error message will
+        /// fall in this line, and be printed to the console, and exit afterwards.
         printf("%s", program.error_message);
         return 1;
     }
 
+    /// Prints the assembly instructions generated
     pcode_print();
     puts("");
+
+    /// Prints the symbol table
     st_print();
 
     free(tokens);
